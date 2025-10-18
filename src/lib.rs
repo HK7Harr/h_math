d66ea8473f8733e3685321af55d854b2d4f376ac
Your Name <henryaakselliergmail.com>
2025-10-19 01:05:28 +0200

    inital commit of hmath

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod traits;
+pub mod prelude;
+
+pub use traits::Average;
---
+++ src/traits.rs
@@ -0,0 +1,36 @@
+pub trait Average {
+    fn h_average(&self) -> f64;
+}
+
+impl Average for [f64] {
+    fn h_average(&self) -> f64 {
+        if self.is_empty() { return 0.0; }
+        let sum: f64 = self.iter().copied().sum();
+        sum / self.len() as f64
+    }
+}
+
+impl Average for Vec<f64> {
+    fn h_average(&self) -> f64 {
+        self.as_slice().h_average()
+    }
+}
+
+impl Average for [i32] {
+    fn h_average(&self) -> f64 {
+        if self.is_empty() { return 0.0; }
+        let sum: i32 = self.iter().copied().sum();
+        sum as f64 / self.len() as f64
+    }
+}
+
+impl Average for Vec<i32> {
+    fn h_average(&self) -> f64 {
+        self.as_slice().h_average()
+    }
+}
+
+pub trait Median {
+    fn h_median() -> f64
+}
+
