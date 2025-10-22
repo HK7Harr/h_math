cb3ef45e10f676684e2474796a620f59244c1802
Your Name <henryaakselliergmail.com>
2025-10-22 16:59:50 +0200

    added length annd temprature conversions, gonna add easy hashmap conversion

--- src/traits.rs
+++ src/traits.rs
@@ -374,5 +374,217 @@ where
     }
 }
 
+// ------------------------------------ Temperature ------------------------------------
+pub trait CelsiusToFahrenheit {
+    fn h_celsius_to_fahrenheit(&self) -> f64;
+}
+impl<T> CelsiusToFahrenheit for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_celsius_to_fahrenheit(&self) -> f64 {
+        (*self).into() * 9.0 / 5.0 + 32.0
+    }
+}
+pub trait FahrenheitToCelsius {
+    fn h_fahrenheit_to_celsius(&self) -> f64;
+}
+impl<T> FahrenheitToCelsius for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_fahrenheit_to_celsius(&self) -> f64 {
+        ((*self).into() - 32.0) * 5.0 / 9.0
+    }
+}   
+
+pub trait CelsiusToKelvin {
+    fn h_celsius_to_kelvin(&self) -> f64;
+}
+impl<T> CelsiusToKelvin for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_celsius_to_kelvin(&self) -> f64 {
+        (*self).into() + 273.15
+    }
+}
+pub trait KelvinToCelsius {
+    fn h_kelvin_to_celsius(&self) -> f64;
+}
+impl<T> KelvinToCelsius for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_kelvin_to_celsius(&self) -> f64 {
+        (*self).into() - 273.15
+    }
+}   
+
+pub trait FahrenheitToKelvin {
+    fn h_fahrenheit_to_kelvin(&self) -> f64;
+}
+impl<T> FahrenheitToKelvin for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_fahrenheit_to_kelvin(&self) -> f64 {   
+        ((*self).into() - 32.0) * 5.0 / 9.0 + 273.15
+    }
+}
+pub trait KelvinToFahrenheit {
+    fn h_kelvin_to_fahrenheit(&self) -> f64;
+}
+impl<T> KelvinToFahrenheit for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_kelvin_to_fahrenheit(&self) -> f64 {
+        ((*self).into() - 273.15) * 9.0 / 5.0 + 32.0
+    }
+}       
+
+
+// ------------------------------------ Length conversions ------------------------------------
+pub trait MetersToKilometers {
+    fn h_meters_to_kilometers(&self) -> f64;
+}
+impl<T> MetersToKilometers for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_meters_to_kilometers(&self) -> f64 {
+        (*self).into() / 1000.0
+    }
+}
+pub trait KilometersToMeters {
+    fn h_kilometers_to_meters(&self) -> f64;
+}
+impl<T> KilometersToMeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_kilometers_to_meters(&self) -> f64 {
+        (*self).into() * 1000.0
+    }
+}       
+
+pub trait CentimetersToMeters {
+    fn h_centimeters_to_meters(&self) -> f64;
+}
+impl<T> CentimetersToMeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_centimeters_to_meters(&self) -> f64 {
+        (*self).into() / 100.0
+    }
+}
+pub trait MetersToCentimeters {
+    fn h_meters_to_centimeters(&self) -> f64;
+}
+impl<T> MetersToCentimeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_meters_to_centimeters(&self) -> f64 {
+        (*self).into() * 100.0
+    }
+}
+
+pub trait CentimetersToMillimeters {
+    fn h_centimeters_to_millimeters(&self) -> f64;
+}
+impl<T> CentimetersToMillimeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_centimeters_to_millimeters(&self) -> f64 {
+        (*self).into() * 10.0
+    }
+}
+pub trait MillimetersToCentimeters {
+    fn h_millimeters_to_centimeters(&self) -> f64;
+}
+impl<T> MillimetersToCentimeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_millimeters_to_centimeters(&self) -> f64 {
+        (*self).into() / 10.0
+    }
+}
+
+pub trait KilometersToMiles {
+    fn h_kilometers_to_miles(&self) -> f64;
+}
+impl<T> KilometersToMiles for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_kilometers_to_miles(&self) -> f64 {
+        (*self).into() * 0.621371
+    }
+}
+pub trait MilesToKilometers {
+    fn h_miles_to_kilometers(&self) -> f64;
+}
+impl<T> MilesToKilometers for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_miles_to_kilometers(&self) -> f64 {
+        (*self).into() / 0.621371
+    }
+}
+
+pub trait InchesToCentimeters {
+    fn h_inches_to_centimeters(&self) -> f64;
+}
+impl<T> InchesToCentimeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_inches_to_centimeters(&self) -> f64 {
+        (*self).into() * 2.54
+    }
+}
+
+pub trait CentimetersToInches {
+    fn h_centimeters_to_inches(&self) -> f64;
+}
+impl<T> CentimetersToInches for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_centimeters_to_inches(&self) -> f64 {
+        (*self).into() / 2.54
+    }
+}
+
+pub trait CentimetersToDecimeters {
+    fn h_centimeters_to_decimeters(&self) -> f64;
+}
+impl<T> CentimetersToDecimeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_centimeters_to_decimeters(&self) -> f64 {
+        (*self).into() / 10.0
+    }
+}
+pub trait DecimetersToCentimeters {
+    fn h_decimeters_to_centimeters(&self) -> f64;
+}
+impl<T> DecimetersToCentimeters for T
+where
+    T: Copy + Into<f64>,
+{
+    fn h_decimeters_to_centimeters(&self) -> f64 {
+        (*self).into() * 10.0
+    }
+}
+
+// --------------------------- functionality
 
 
